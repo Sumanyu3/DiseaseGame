59e6104c4732dbe86c309022a629fc7e6d9931fa
Sumanyu3 <sumanyu18106@iiitd.ac.in>
2019-05-27 14:02:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/disease_game_supergirl/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.disease_game_supergirl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
